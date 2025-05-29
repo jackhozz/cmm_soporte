0ad9eab698d98447f0105edd4525e66044fdbcc3
jackhozz <jackson.zerpa.13@gmail.com>
2025-05-29 08:33:55 -0400

    Update Admin. Tickets.py

--- pages/Admin. Tickets.py
+++ pages/Admin. Tickets.py
@@ -2,7 +2,7 @@ import streamlit as st
 import login as login
 
 archivo=__file__.split("\\")[-1]
-login.generarLogin(archivo)
+login.main.generarLogin(archivo)
 if 'usuario' in st.session_state:
     st.header('Página :orange[principal]')
     st.subheader('Información página principal')
