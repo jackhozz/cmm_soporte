40f9323de93e83cf783587325f0955b91299db1d
jackhozz <jackson.zerpa.13@gmail.com>
2025-05-29 08:31:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import streamlit as st
-import login as login
+def main():
+    import streamlit as st
+    import login as login
+    
+    archivo=__file__.split("\\")[-1]
+    login.generarLogin(archivo)
+    if 'usuario' in st.session_state:
+        st.header('Página :orange[principal]')
+        st.subheader('Información página principal')
 
-archivo=__file__.split("\\")[-1]
-login.generarLogin(archivo)
-if 'usuario' in st.session_state:
-    st.header('Página :orange[principal]')
-    st.subheader('Información página principal')
+if __name__ == "__main__" :
+    main()
